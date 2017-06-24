8831717db00867c582d0dc29f68a457f31202ace
Hien Le <hienlt0610@gmail.com>
2017-06-24 08:19:22 +0700

    [hienlt0610] + Update UI

--- app/src/main/java/smartbook/hutech/edu/smartbook/ui/activity/MainActivity.java
+++ app/src/main/java/smartbook/hutech/edu/smartbook/ui/activity/MainActivity.java
@@ -1,14 +1,18 @@
 package smartbook.hutech.edu.smartbook.ui.activity;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.ViewPager;
 
 import butterknife.BindView;
 import smartbook.hutech.edu.smartbook.R;
 import smartbook.hutech.edu.smartbook.adapter.TabPageAdapter;
 import smartbook.hutech.edu.smartbook.common.BaseActivity;
-import smartbook.hutech.edu.smartbook.ui.fragment.HomeFragment;
 
 public class MainActivity extends BaseActivity {
 
@@ -34,12 +38,17 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initTabLayoutAndPager() {
-        mTabPageAdapter = new TabPageAdapter(getSupportFragmentManager());
-        mTabPageAdapter.addPage("test", new HomeFragment());
-        mTabPageAdapter.addPage("test", new HomeFragment());
-        mTabPageAdapter.addPage("test", new HomeFragment());
-        mViewPager.setAdapter(mTabPageAdapter);
-        mTabLayout.setupWithViewPager(mViewPager);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher) // notification icon
+                .setContentTitle("Notification!") // title for notification
+                .setContentText("Hello word") // message for notification
+                .setAutoCancel(true); // clear notification after click
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        mBuilder.setContentIntent(pi);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, mBuilder.build());
     }
 
     @Override
