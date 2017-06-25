5cf2bc111cec254e1d8ef7d7e8b2fca224ecfdbc
Hien Le <hienlt0610@gmail.com>
2017-06-25 18:05:05 +0700

    [hienlt0610] * update .gitignore

---
+++ app/src/main/java/smartbook/hutech/edu/smartbook/common/view/ViewTest.java
@@ -0,0 +1,8 @@
+package smartbook.hutech.edu.smartbook.common.view;
+
+/**
+ * Created by hienl on 6/25/2017.
+ */
+
+public class ViewTest {
+}
---
+++ app/src/main/java/smartbook/hutech/edu/smartbook/ui/activity/TestActivity.java
@@ -0,0 +1,8 @@
+package smartbook.hutech.edu.smartbook.ui.activity;
+
+/**
+ * Created by hienl on 6/25/2017.
+ */
+
+public class TestActivity {
+}
